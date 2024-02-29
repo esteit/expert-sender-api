06e737df78eef758ff181ef2bf82938b873aa873
Alexander Perov <alexander.perov@gmail.com>
2024-02-29 11:01:47 +0300

    Update ResponseReceivedEvent.php

--- src/Event/ResponseReceivedEvent.php
+++ src/Event/ResponseReceivedEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Citilink\ExpertSenderApi\Event;
 
 use Citilink\ExpertSenderApi\ResponseInterface;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Event after response received
